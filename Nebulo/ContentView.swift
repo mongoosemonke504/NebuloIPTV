9d6793ac550e8c71b1b9aa3c7de398a1a9e7998c
Robert Hillhouse <robbhillhouse1@gmail.com>
2025-11-29 13:47:38 -0500

    Initial Commit

---
+++ Nebulo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nebulo
+//
+//  Created by Robert Hillhouse on 11/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nebulo/NebuloApp.swift
@@ -0,0 +1,17 @@
+//
+//  NebuloApp.swift
+//  Nebulo
+//
+//  Created by Robert Hillhouse on 11/29/25.
+//
+
+import SwiftUI
+
+@main
+struct NebuloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
