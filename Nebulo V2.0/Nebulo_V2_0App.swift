09f861ea21072c39f8aa1b4e1d81d6013c4a0fe3
Robert Hillhouse <robbhillhouse1@gmail.com>
2025-12-21 18:50:09 -0500

    Initial Commit

---
+++ Nebulo V2.0/Nebulo_V2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Nebulo_V2_0App.swift
+//  Nebulo V2.0
+//
+//  Created by Robert Hillhouse on 12/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Nebulo_V2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
