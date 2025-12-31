f4d848d42fd7f042a6c6c778ea93bb4b94311140
mongoosemonke <robbhillhouse3@gmail.com>
2025-12-31 10:51:26 -0500

    Initial Commit

---
+++ Nebulo_V2.1/Nebulo_V2_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Nebulo_V2_1App.swift
+//  Nebulo_V2.1
+//
+//  Created by Robert Hillhouse on 12/31/25.
+//
+
+import SwiftUI
+
+@main
+struct Nebulo_V2_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
