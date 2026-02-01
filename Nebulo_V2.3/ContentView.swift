419f77ebdba70f7e89faeca5584a27639a5dfaae
mongoosemonke504 <robbhillhouse3@gmail.com>
2026-02-01 00:47:34 -0500

    Initial Commit

---
+++ Nebulo_V2.3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nebulo_V2.3
+//
+//  Created by Robert Hillhouse on 2/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
